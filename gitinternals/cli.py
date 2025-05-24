43093b843f7896b5e47205084ea350581ab3c0b6
CoYildiz <127496796+CoYildiz@users.noreply.github.com>
2025-05-24 03:44:40 +0300

    Upload data.py and ugit.py and setup.py
    Includes initial ugit CLI setup and commands:
    - init
    - hash-object
    - cat-file
    Based on the guide at https://www.leshenko.net/p/ugit/

---
+++ gitinternals/cli.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+import sys
+
+from . import data
+
+def main():
+    args = parse_args()
+    args.func(args)
+
+def parse_args():        #  Komut satırı argümanlarını ayrıştırmak için yardımcı fonksiyon.
+    parser = argparse.ArgumentParser() 
+
+    commands = parser.add_subparsers(dest="command")
+    commands.required = True
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init) # Eğer kullanıcı init komutunu kullanırsa, çalıştırılacak fonksiyon olarak init fonksiyonu atanır.
+
+    #ugit.py hash-object <dosya> şeklinde yeni bir komut tanımlar## Yukaridaki init gibi
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)# tanimli olan hash_object fonksiyonunu calistirir
+    hash_object_parser.add_argument("file")# Burada farkli olan komutun bir tane zorunlu arguman alacagini belirtmesi:file
+
+    #ugit.py cat-file <object> şeklindeki komutları tanır 
+    #This command is the "opposite" of hash-object:it can print an object bu its OID.
+    cat_file_parser = commands.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
+
+
+    return parser.parse_args()
+ 
+
+def init(args):
+    data.init()
+    print(f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
+
+
+def hash_object(args):
+    with open(args.file, "rb") as f:
+        print(data.hash_object(f.read()))
+
+
+def cat_file (args):
+    sys.stdout.flush ()
+    sys.stdout.buffer.write (data.get_object (args.object))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
